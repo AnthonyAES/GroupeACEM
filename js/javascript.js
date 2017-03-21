01829c2fe2d278e0daee200e53ada6162f1dd0f8
mélysa <melissa.bojama@gmail.com>
2017-03-21 16:04:36 +0100

    site fini

---
+++ js/javascript.js
@@ -0,0 +1,19 @@
+var lvl1 = document.getElementById('lvl1');
+lvl1.addEventListener('mouseover', function () {
+    this.classList.add('hovered');
+});
+
+var lvl2 = document.getElementById('lvl2');
+lvl2.addEventListener('mouseover', function () {
+    this.classList.add('hovered');
+});
+
+var lvl3 = document.getElementById('lvl3');
+lvl3.addEventListener('mouseover', function () {
+    this.classList.add('hovered');
+});
+
+var lvl4 = document.getElementById('lvl4');
+lvl4.addEventListener('mouseover', function () {
+    this.classList.add('hovered');
+});
